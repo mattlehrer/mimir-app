eefbc6517a511a807fbb0d63d24fc791150ff8b5
Matt Lehrer <lehrerm@gmail.com>
2022-08-11 11:01:31 +0200

    setup supabase auth wip

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
+	import { session } from '$app/stores';
+	import { supabaseClient } from '$lib/db';
 	import Footer from '$lib/Footer.svelte';
 	import Nav from '$lib/Nav.svelte';
+	import { SupaAuthHelper } from '@supabase/auth-helpers-svelte';
 
 	import '../app.css';
 </script>
@@ -8,7 +11,13 @@
 <Nav />
 
 <main>
+	{#if supabaseClient}
+	<SupaAuthHelper {supabaseClient} {session}>
+		<slot />
+	</SupaAuthHelper>
+	{:else}
 	<slot />
+	{/if}
 </main>
 
 <Footer />
