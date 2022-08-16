7c5d553278ab42f0aff5b09535880ec97bf3b6b3
Matt Lehrer <lehrerm@gmail.com>
2022-08-16 12:25:52 +0200

    add skeleton components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/@brainandbones/skeleton/**/*.{html,js,svelte,ts}',
+	],
 
 	theme: {
 		extend: {},
 	},
 
-	plugins: [require('@tailwindcss/forms')],
+	plugins: [require('@tailwindcss/forms'), require('@brainandbones/skeleton/tailwind.cjs')],
 };
 
 module.exports = config;
