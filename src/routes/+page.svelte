f17c72df48916816770dc50a861634427902f503
Matt Lehrer <lehrerm@gmail.com>
2022-08-16 12:25:39 +0200

    prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,19 +22,18 @@
 		</div>
 		<div class="mt-8 flex w-full justify-center">
 			<div class="flex flex-col gap-8">
-
 				<a
-				href="/api/oauth/google"
-				class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2 text-center"
-				>Add a Google Analytics account</a
+					href="/api/oauth/google"
+					class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-center text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
+					>Add a Google Analytics account</a
 				>
 				<button
-				class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
-				on:click={getReport}
+					class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
+					on:click={getReport}
 				>
-				Get Report
-			</button>
+					Get Report
+				</button>
+			</div>
 		</div>
-		</div>
-		{/if}
+	{/if}
 </div>
