66f1c2b33769e695d2300cff32c9e5799db52db3
Matt Lehrer <lehrerm@gmail.com>
2022-08-16 16:02:00 +0200

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,23 +16,27 @@
 	{#if !$session?.user}
 		<SignUp />
 	{:else}
-		<div class="w-full text-center text-2xl">
-			Hi, {$session.user.email}
-			{JSON.stringify($session.user, null, 2)}
-		</div>
-		<div class="mt-8 flex w-full justify-center">
-			<div class="flex flex-col gap-8">
-				<a
-					href="/api/oauth/google"
-					class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-center text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
-					>Add a Google Analytics account</a
-				>
-				<button
-					class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
-					on:click={getReport}
-				>
-					Get Report
-				</button>
+		<div class="mx-auto mt-16 max-w-3xl">
+			<div class="w-full text-center text-2xl">
+				Hi, {$session.user.email}
+				<p class="my-8">
+					{JSON.stringify($session.user, null, 2)}
+				</p>
+			</div>
+			<div class="mt-8 flex w-full justify-center">
+				<div class="flex flex-col gap-8">
+					<a
+						href="/api/oauth/google"
+						class="btn inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-center text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
+						>Add a Google Analytics account</a
+					>
+					<button
+						class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
+						on:click={getReport}
+					>
+						Get Report
+					</button>
+				</div>
 			</div>
 		</div>
 	{/if}
