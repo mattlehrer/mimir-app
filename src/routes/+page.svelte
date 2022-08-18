6c8b08c4d76b45ffb2b6bf8cad935476e4f06f64
Matt Lehrer <lehrerm@gmail.com>
2022-08-18 15:44:05 +0200

    link to dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,16 +26,10 @@
 			<div class="mt-8 flex w-full justify-center">
 				<div class="flex flex-col gap-8">
 					<a
-						href="/api/oauth/google"
-						class="btn inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-center text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
-						>Add a Google Analytics account</a
+						href="/dashboard"
+						class="btn inline-flex place-content-center rounded-md border border-transparent bg-accent-300 px-4 py-2 text-center text-sm font-medium text-primary-500 shadow-sm hover:bg-accent-500 focus:outline-none focus:ring-2 focus:ring-accent-300 focus:ring-offset-2"
+						>Go to dashboard</a
 					>
-					<button
-						class="inline-flex place-content-center rounded-md border border-transparent bg-[#C5A87A] px-4 py-2 text-sm font-medium text-[#1C3E4F] shadow-sm hover:bg-[#d8c5a6] focus:outline-none focus:ring-2 focus:ring-[#e8dcca] focus:ring-offset-2"
-						on:click={getReport}
-					>
-						Get Report
-					</button>
 				</div>
 			</div>
 		</div>
