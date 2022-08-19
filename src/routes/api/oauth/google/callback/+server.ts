6b24ef4e2d3b292ec67bd5d664d03d68f718266c
Matt Lehrer <lehrerm@gmail.com>
2022-08-19 13:18:48 +0200

    use rls

--- src/routes/api/oauth/google/callback/+server.ts
+++ src/routes/api/oauth/google/callback/+server.ts
@@ -1,51 +1,88 @@
 import { oauth2Client } from '$lib/google';
-import { supabaseClient } from '$lib/supabase';
-import type { RequestHandler } from '@sveltejs/kit';
-import cookie from 'cookie';
+import { supabaseServerClient, withApiAuth } from '@supabase/auth-helpers-sveltekit';
+import { error, redirect } from '@sveltejs/kit';
 import jwt_decode from 'jwt-decode';
+import type { RequestHandler } from './$types';
 
-/** @type {import('@sveltejs/kit').RequestHandler} */
-export const GET: RequestHandler = async function ({ request, url }) {
+export const GET: RequestHandler = async function ({ url, locals }) {
 	console.log('searchParams:', url.searchParams.toString());
 	const code = url.searchParams.get('code');
 	if (!code) {
 		return new Response('Missing code', { status: 400 });
 	}
-	let tokens;
+	let tokens: any;
 	try {
-		// ({ tokens } = await oauth2Client.getToken(code));
-		const data = await oauth2Client.getToken(code);
-		console.log('getToken response:', JSON.stringify(data, null, 2));
-		({ tokens } = data);
-	} catch (error) {
+		({ tokens } = await oauth2Client.getToken(code));
+		// const data = await oauth2Client.getToken(code);
+		// console.log('getToken response:', JSON.stringify(data, null, 2));
+		// ({ tokens } = data);
+	} catch (err) {
 		console.error('Google Oauth error: ', JSON.stringify(error, null, 2));
-		if (error instanceof Error) {
-			return new Response(error.message, { status: 500 });
+		if (err instanceof Error) {
+			throw error(500, err.message);
+			// return new Response(error.message, { status: 500 });
 		} else {
-			return new Response(JSON.stringify(error), { status: 500 });
+			throw error(500, JSON.stringify(err, null, 2));
 		}
 	}
-	// console.log(JSON.stringify(tokens, null, 2));
-	if (!supabaseClient) return new Response('Missing supabaseClient', { status: 500 });
-	const cookies = cookie.parse(request.headers.get('cookie') ?? '');
-	const jwt = cookies['sb-access-token'] ?? '';
-	const uid = (await supabaseClient.auth.api.getUser(jwt))?.user?.id;
-	if (!uid) return new Response('No user', { status: 500 });
-	const decodedGoogleJWT = tokens.id_token && (jwt_decode(tokens.id_token) as any);
-	const data = await supabaseClient.from('google_tokens').upsert(
-		{
-			user: uid,
-			email: decodedGoogleJWT?.email ?? null,
-			...tokens,
-		},
-		{ onConflict: 'user, email' },
-	);
-	console.log(JSON.stringify(data, null, 2));
 
-	return new Response(undefined, {
-		status: 303,
-		headers: {
-			location: '/',
+	return await withApiAuth(
+		{
+			redirectTo: '/',
+			user: locals.user,
 		},
-	});
+		async () => {
+			if (!locals.accessToken) throw error(401, 'Unauthorized');
+			const decodedGoogleJWT = tokens.id_token && (jwt_decode(tokens.id_token) as any);
+			const { error: err } = await supabaseServerClient(locals.accessToken)
+				.from<GoogleTokensTable>('google_tokens')
+				.upsert(
+					{
+						user_id: locals.user.id,
+						email: decodedGoogleJWT?.email ?? null,
+						...tokens,
+					},
+					{ onConflict: 'user_id, email' },
+				);
+			if (err) console.error(err);
+
+			throw redirect(303, '/dashboard');
+		},
+	);
+	// console.log(JSON.stringify(tokens, null, 2));
+	// if (!supabaseClient) return new Response('Missing supabaseClient', { status: 500 });
+	// const cookies = cookie.parse(request.headers.get('cookie') ?? '');
+	// const jwt = cookies['sb-access-token'] ?? '';
+	// const uid = (await supabaseClient.auth.api.getUser(jwt))?.user?.id;
+	// if (!uid) return new Response('No user', { status: 500 });
+	// const decodedGoogleJWT = tokens.id_token && (jwt_decode(tokens.id_token) as any);
+	// const { error } = await supabaseClient.from('google_tokens').upsert(
+	// 	{
+	// 		user: uid,
+	// 		email: decodedGoogleJWT?.email ?? null,
+	// 		...tokens,
+	// 	},
+	// 	{ onConflict: 'user_id, email' },
+	// );
+	// if (error) console.error(error);
+	// // console.log(JSON.stringify(data, null, 2));
+
+	// return new Response(undefined, {
+	// 	status: 303,
+	// 	headers: {
+	// 		location: '/',
+	// 	},
+	// });
 };
+
+interface GoogleTokensTable {
+	id: string;
+	user_id: string;
+	email: string;
+	access_token: string;
+	refresh_token: string;
+	scope: string;
+	token_type: string;
+	id_token: string;
+	expiry_date: number;
+}
