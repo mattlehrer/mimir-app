9c84d9c7a55de3da20f91e0da97486ad76d3511c
Matt Lehrer <lehrerm@gmail.com>
2022-08-24 16:05:04 +0200

    add accordion - wip, jank on 0->1, 1->0

--- src/routes/dashboard/report/[id]/+page.svelte
+++ src/routes/dashboard/report/[id]/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+	import { AccordionGroup, AccordionItem } from '@brainandbones/skeleton';
 	import growth_opportunity from 'assets/growth_opportunity.png';
 	import { onMount } from 'svelte';
 	import { bounceOut } from 'svelte/easing';
 	import { tweened } from 'svelte/motion';
 	import Arc from './Arc.svelte';
 	import Needle from './Needle.svelte';
+	import { writable, type Writable } from 'svelte/store';
+	let storeAccordion: Writable<number[]> = writable([0]);
 
 	export let results = {
 		icon: 'blob:https://www.canva.com/619f4971-c97a-4703-8e99-9a246e67abcc',
 		recommendation: 'Write a shorter <span class="font-mono font-extrabold text-accent-600">/about</span> page',
-		why: 'It is important to share to your customers who you are and what your are about, it can build trust and a more personal connection between your customers and your brand.',
+		why: 'It is important to share to your customers who you are and what you are about, it can build trust and a more personal connection between your customers and your brand.',
 		data: 'We found that customers spend a very short time on your about page, which is not enough time for them to actually read about who you are. Therefore we suggest that you write a shorter text for the about page.',
 		help: 'Need some help with that? We have a partner who is an expert copywriter who can help with making text concise and to the point.',
 		partner: { name: 'lemonscentedtea', website: 'https://lemonscentedtea.com' },
@@ -33,52 +36,62 @@
 
 <article class="relative mx-auto mt-4 mb-16 flex max-w-lg flex-col justify-center px-4 md:mt-0">
 	<div class="absolute top-0 hidden items-start md:block">
-		<img src={growth_opportunity} class="opacity-20 lg:opacity-50" alt="growth opportunity" />
+		<img src={growth_opportunity} class="z-0 opacity-20 lg:opacity-50" alt="growth opportunity" />
 	</div>
-	<div class="flex flex-col space-y-6 text-accent-300">
+	<div class="z-10 flex flex-col space-y-6 text-lg text-accent-300">
 		<h1 class="text-4xl font-semibold">{@html results.recommendation}</h1>
-		<div>
-			<h2 class="text-base font-semibold">Why?</h2>
-			<p class="text-base">{results.why}</p>
-		</div>
-		<div>
-			<h2 class="text-base font-semibold">The Data</h2>
-			<p class="text-base">{results.data}</p>
-		</div>
-		<div>
-			<h2 class="text-base font-semibold">Help</h2>
-			<p class="text-base">{results.help}</p>
-			<p class="text-base">
-				Reach out to <a class=" text-accent-800" href={results.partner.website}>{results.partner.name}</a>
-			</p>
-		</div>
-		<div class="flex justify-center py-4">
-			<figure class="relative flex flex-col items-center space-y-6">
-				<Arc />
-				<div
-					id="needle"
-					style="--tw-rotate: {`${-90 + $gaugeAngle}deg`};"
-					class="absolute bottom-0 h-48 w-auto pt-2 text-accent-600"
-				>
-					<Needle />
+		<div class="w-full">
+			<AccordionGroup classes="font-extralight" selected={storeAccordion} spacing="space-y-4" multiple>
+				<AccordionItem class="-space-y-1" value={0}>
+					<h2 slot="title">Why?</h2>
+					<svelte:fragment slot="description">
+						{results.why}
+					</svelte:fragment>
+				</AccordionItem>
+				<AccordionItem class="-space-y-1" value={1}>
+					<h2 slot="title">The Data</h2>
+					<p slot="description">
+						{results.data}
+					</p>
+				</AccordionItem>
+				<AccordionItem class="-space-y-1" value={2}>
+					<h2 slot="title">Help</h2>
+					<div slot="description">
+						<p>
+							{results.help}
+						</p>
+						<p>
+							Reach out to <a class=" text-accent-800" href={results.partner.website}>{results.partner.name}</a>
+						</p>
+					</div>
+				</AccordionItem>
+				<div class="flex justify-center py-4">
+					<figure class="relative flex flex-col items-center space-y-6">
+						<Arc />
+						<div
+							id="needle"
+							style="--tw-rotate: {`${-90 + $gaugeAngle}deg`};"
+							class="absolute bottom-0 h-48 w-auto pt-2 text-accent-600"
+						>
+							<Needle />
+						</div>
+						<figcaption class="max-w-xs px-8 pt-2 text-center">{results.gauge.label}</figcaption>
+					</figure>
 				</div>
-				<figcaption class="max-w-xs px-8 pt-2 text-center">{results.gauge.label}</figcaption>
-			</figure>
+			</AccordionGroup>
 		</div>
 	</div>
 </article>
 
 <style>
-	p {
-		@apply font-extralight;
-	}
-	h2,
-	p {
-		@apply text-lg;
-	}
 	h2 {
-		@apply underline;
+		@apply font-semibold underline;
 	}
+
+	p {
+		@apply mt-0;
+	}
+
 	:is(#article, article) a {
 		@apply font-normal text-inherit underline decoration-accent-300/75 decoration-dotted underline-offset-4;
 	}
