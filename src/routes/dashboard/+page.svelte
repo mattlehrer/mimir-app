2e3bf9ee8a7549ad62f43107d0b5f24957b4689f
Matt Lehrer <lehrerm@gmail.com>
2022-08-25 14:08:28 +0200

    eliminate unnecessary load

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import type { analytics_v3 } from 'googleapis';
+	import { page } from '$app/stores';
 	import ViewTable from './ViewTable.svelte';
 
-	export let data: {
-		analyticsViews: Array<analytics_v3.Schema$Profile & { account: { id: string; name: string; email: string } }>;
-		activeViews: Array<{ id: string; view_id: string; active: boolean; uses_report: string }>;
-	};
+	const { data } = $page;
 </script>
 
-<ViewTable {...data} />
+<ViewTable analyticsViews={data.analyticsViews} />
